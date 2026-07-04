9e193bcfbdf292c8c58c7b8490fab33d951f89a5
GeovaneeSantos <geovane.geors@gmial.com>
2026-07-04 20:44:40 -0300

    feat: Add favicon to the project and clean up header.php

--- config/header.php
+++ config/header.php
@@ -15,9 +15,10 @@ if (!isset($_SESSION['user_name']) && basename($_SERVER['SCRIPT_NAME']) != "logi
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    
     <title>Inovatec - Login</title>
 
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
